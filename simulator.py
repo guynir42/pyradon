39b6ea6424fef4d000094f552d46a5fd7d74e232
Pawel Nerka <pnerka@sybillatechnologies.com>
2021-03-10 11:49:27 +0100

    Fix problem with imports + modify example in jupiter notebook

--- simulator.py
+++ simulator.py
@@ -26,185 +26,192 @@ import scipy.signal
 import numpy as np
 import math
 
-from utils import empty, scalar, imsize, gaussian2D
-import pyradon.finder
+from utils import empty, scalar, imsize, gaussian2D, model
+from finder import Finder
+
 
 class Simulator:
     def __init__(self):
-        
+
         # objects
-        self.finder = pyradon.finder.Finder()
-        
+        self.finder = Finder()
+
         # outputs
         self.image_clean = []
         self.image = []
         self.psf = []
-        
+
         # switches
-        self.im_size = 512 # assume we want to make square images
+        self.im_size = 512  # assume we want to make square images
         self.intensity = 10
         self.bg_noise_var = 1
         self.use_source_noise = True
         self.psf_sigma = 2
-        
+
         # these parameters are in units of image size
         self.x1 = 0
         self.x2 = 1
         self.y1 = 0
         self.y2 = 1
-    
+
         self.debug_bit = 1
-        
+
     @property
     def L(self):
         # return math.sqrt((self.x2-self.x1)**2+(self.y2-self.y1)**2)
-        x1 = min(max(self.x1,0),1)
-        x2 = min(max(self.x2,0),1)
-        y1 = min(max(self.y1,0),1)
-        y2 = min(max(self.y2,0),1)
+        x1 = min(max(self.x1, 0), 1)
+        x2 = min(max(self.x2, 0), 1)
+        y1 = min(max(self.y1, 0), 1)
+        y2 = min(max(self.y2, 0), 1)
 
         return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
     @property
     def th(self):
         return math.degrees(math.atan2(self.y2-self.y1, self.x2-self.x1))
-    
+
     @property
     def a(self):
-        if self.x1==self.x2:
+        if self.x1 == self.x2:
             return float('NaN')
         else:
             return (self.y2-self.y1)/(self.x2-self.x1)
-    
+
     @property
     def b(self):
         return self.y2-self.a*self.x2
-    
+
     @property
     def x0(self):
-        if self.x1==self.x2:
+        if self.x1 == self.x2:
             return self.x1
-        elif self.a==0:
-            return float('NaN')            
+        elif self.a == 0:
+            return float('NaN')
         else:
-            return -self.b/self.a 
-        
+            return -self.b/self.a
+
     @property
     def midpoint_x(self):
         return (self.x2+self.x1)/2
-     
+
     @property
     def midpoint_y(self):
         return (self.y2+self.y1)/2
-    
+
     @property
     def trig_factor(self):
         return max(math.fabs(math.cos(math.radians(self.th))), math.fabs(math.sin(math.radians(self.th))))
-    
+
     def clear(self):
         self.image_clean = []
         self.image = []
         self.psf = []
-        
+
     def isVertical(self):
-        
-        val1 = self.th>=45 and self.th<=135
-        val2 = self.th>=-135 and self.th<=-45
-        
+
+        val1 = self.th >= 45 and self.th <= 135
+        val2 = self.th >= -135 and self.th <= -45
+
         return val1 or val2
-    
+
     def calcSNR(self):
 
         snr = self.intensity*math.sqrt(self.L*self.im_size/self.bg_noise_var)
-        
+
         snr = snr/math.sqrt(2*math.sqrt(math.pi)*self.psf_sigma)
-    
+
         return abs(snr)
 
     def makeImage(self):
 
-        if self.debug_bit > 1: print("makeImage()")
+        if self.debug_bit > 1:
+            print("makeImage()")
 
         self.clear()
         self.makeClean()
         self.addNoise()
 
     def makeClean(self):
-        self.image_clean = self.intensity*pyradon.utils.model(self.im_size, self.x1*self.im_size,
-                                                              self.x2*self.im_size, self.y1*self.im_size,
-                                                              self.y2*self.im_size, self.psf_sigma)
+        self.image_clean = self.intensity*model(self.im_size, self.x1*self.im_size,
+                                                self.x2*self.im_size, self.y1*self.im_size,
+                                                self.y2*self.im_size, self.psf_sigma)
 
     def addNoise(self):
-        
-        if self.debug_bit>2: print("addNoise()")
-           
+
+        if self.debug_bit > 2:
+            print("addNoise()")
+
         bg = np.ones(imsize(self.im_size), dtype="float32")*self.bg_noise_var
-        
+
         if self.use_source_noise:
             var = bg + np.abs(self.image_clean)
         else:
             var = bg
-        
+
         self.image = np.random.normal(self.image_clean, np.sqrt(var)).astype('float32')
-        
+
     def find(self):
-        
-        if self.debug_bit>1: print("find()")
+
+        if self.debug_bit > 1:
+            print("find()")
 #        self.image_final = np.concatenate((self.image_final ,np.zeros((self.image_final.shape[0],300))),1) # this is just for testing non-square input images...
         #self.finder.input_psf = self.psf_sigma
         #self.finder.input_var = self.bg_noise_var
-        #self.finder.findSingle(self.image)
+        # self.finder.findSingle(self.image)
         self.finder.input(self.image, psf=self.psf_sigma, variance=self.bg_noise_var)
-        
+
     def run(self):
-        
-        if self.debug_bit>1: print("run()")
+
+        if self.debug_bit > 1:
+            print("run()")
         self.clear()
         self.makeImage()
         self.find()
-        
+
         if empty(self.finder.streaks):
             # print("No streaks found. Maximal S/N= %f" % max(np.max(self.finder.radon_image), np.max(self.finder.radon_image_trans)))
             print("No streaks found. Maximal S/N= %f" % self.finder.best_SNR)
         else:
 
             s = self.finder.streaks[0]
-            
-            if self.debug_bit:            
-                print("SIMULATED : S/N= %4.2f | I= %5.2f | L= %6.1f | th= %4.2f | x0= %4.1f" % (self.calcSNR(), self.intensity, self.L*self.im_size, self.th, self.x0*self.im_size))
-                print("CALCULATED: S/N= %4.2f | I= %5.2f | L= %6.1f | th= %4.2f | x0= %4.1f" % (s.snr, s.I, s.L, s.th, s.x0))
-    
-            if self.debug_bit>1:
-    
+
+            if self.debug_bit:
+                print("SIMULATED : S/N= %4.2f | I= %5.2f | L= %6.1f | th= %4.2f | x0= %4.1f" %
+                      (self.calcSNR(), self.intensity, self.L*self.im_size, self.th, self.x0*self.im_size))
+                print("CALCULATED: S/N= %4.2f | I= %5.2f | L= %6.1f | th= %4.2f | x0= %4.1f" %
+                      (s.snr, s.I, s.L, s.th, s.x0))
+
+            if self.debug_bit > 1:
+
                 input_xy = (self.x1, self.x2, self.y1, self.y2)
                 input_xy = tuple((int(round(x*self.im_size)) for x in input_xy))
                 print("INPUT: x1= % 4d | x2= % 04d | y1= % 4d | y2= % 4d" % input_xy)
-                
+
                 print("FOUND: x1= % 4d | x2= % 04d | y1= % 4d | y2= % 4d" % (s.x1, s.x2, s.y1, s.y2))
-                
-                
+
+
 # test (reload object)
 if __name__ == "__main__":
-    
-    import matplotlib.pyplot as plt    
-    
+
+    import matplotlib.pyplot as plt
+
     print("this is a test for Simulator and Finder...")
-    
+
     if 's' not in locals() or not isinstance(s, Simulator):
         s = Simulator()
-        s.debug_bit=1
-    
+        s.debug_bit = 1
+
     s.x1 = 3.0/8
     s.x2 = 0.5
     s.y1 = 1/3
     s.y2 = 1.0/2
-    
+
     s.x1 = 0.2
     s.y1 = 0.01
     s.x2 = 1
     s.y2 = 1.5
 
-    s.finder.use_subtract_mean=0
+    s.finder.use_subtract_mean = 0
     s.run()
 
     fig, ax = plt.subplots()
@@ -212,6 +219,3 @@ if __name__ == "__main__":
     if not empty(s.finder.streaks):
         for st in s.finder.streaks:
             st.plotLines(ax)
-
-    
-    
--- streak.py
+++ streak.py
@@ -11,60 +11,63 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pyradon.utils import empty
-from pyradon.utils import imsize
-from pyradon.utils import model
+from utils import empty, imsize, model
+
 
 class Streak:
     """
      This object describes a single streak that is found using the FRT, 
      including the raw results of the finding algorithm, and the streak 
      parameters derived from the results.
-     
+
      Usually a Finder object will have a list of these objects saved 
      after it has finished going over some images. 
-    
+
      The Streak object can optionally keep a copy of the original image and 
      the Radon image where it was detected, as well as housekeeping data such
      as the filename, batch number and frame number of that image. 
-    
+
      The Streak object can also be used to subtract the streak from the input image. 
-     
+
     """
-    
+
     def __init__(self, snr=None, transpose=False, threshold=None, peak=None, foldings=None, subframe=None, section=None):
 
         self.im_size = None  # size of original image. updated directly from finder
 
         self.image = None  # subtracted image, as given to finder. updated directly from finder
         self.radon_image = None  # full Radon image for the correct transposition. updated directly from finder
-        self.subframe = subframe  # subframe where streak is detected. For non-short streaks, equal to "radon_image".
-        self.image_section_raw = None  # smaller region of the original image, or just equal to original image (if no sectioning is done)
-        self.image_section_proc = section  # same as section, only PSF filtered, and streak subtracted (all processing is applied)
-        self.corner_section = (0,0)  # the corner of the section inside the full image
+        # subframe where streak is detected. For non-short streaks, equal to "radon_image".
+        self.subframe = subframe
+        # smaller region of the original image, or just equal to original image (if no sectioning is done)
+        self.image_section_raw = None
+        # same as section, only PSF filtered, and streak subtracted (all processing is applied)
+        self.image_section_proc = section
+        self.corner_section = (0, 0)  # the corner of the section inside the full image
         self.size_section = None  # size of section (2-tuple)
-        self.size_section_tr = None  # size of section after transpose (if not transposed, equal to size_section)
+        # size of section after transpose (if not transposed, equal to size_section)
+        self.size_section_tr = None
         self.image_cutout = None  # small region around streak
-        self.cut_size = 128 
+        self.cut_size = 128
         # self.image_cutout_proc = None  # small region around streak with full processing applied
         self.corner_cutout = None  # the corner of the cutout inside the full image
         self.size_cutout = None  # size of the cutout (2-tuple)
         self.psf = None  # the PSF image itself, normalized, that was used to do the filter. updated directly from finder
-        
+
         # these help find the correct image where the streak exists
         self.frame_num = None  # which frame in the batch. updated directly from finder
         self.batch_num = None  # which batch in the run. updated directly from finder
-        self.filename = ''  # which file it came from. updated directly from finder        
+        self.filename = ''  # which file it came from. updated directly from finder
 
         self.threshold = threshold  # which threshold was used. updated directly from finder
         self.is_short = None  # if yes, subframe is smaller than the full Radon image. updated in "calculate()"
-        
+
         # how bright the streak was
-        self.I = None # intensity, or brightness per unit length. found in "calculate()"
-        self.snr = snr # calculated from subframe and index. found in "calculate()"
+        self.I = None  # intensity, or brightness per unit length. found in "calculate()"
+        self.snr = snr  # calculated from subframe and index. found in "calculate()"
         self.snr_fwhm = None  # S/N per resolution element
         self.count = None  # how many photons across the whole streak.
-        
+
         # calculated parameters
         self.L = None  # length of streak (pixels). from "calculate()"
         self.th = None  # angle of streak (th=0 is on the y axis). from "calculate()"
@@ -81,7 +84,8 @@ class Streak:
         # raw coordinates from the Radon result / subframe
         self.transposed = transpose  # was the image transposed? from function argument
         self.foldings = foldings  # in what step in the FRT the streak was found. from function argument
-        self.radon_max_idx = peak  # 3D index of the maximum of the subframe (streak position). from function argument
+        # 3D index of the maximum of the subframe (streak position). from function argument
+        self.radon_max_idx = peak
         self.radon_x0 = None  # position coordinate in the Radon image. using subframe and index
         self.radon_dx = None  # slope coordinate in the Radon image. using subframe and index
         self.radon_x_var = None  # error estimate on "radon_x"
@@ -89,18 +93,21 @@ class Streak:
         self.radon_xdx_cov = None  # cross correlation of the slope-position errors
         self.radon_y1 = None  # start of the subframe
         self.radon_y2 = None  # end of the subframe
-        
+
         # switches and parameters for user
         self.noise_var = None  # updated directly from finder
         self.psf_sigma = None  # updated directly from finder
-        self.subtract_psf_widths = 3  # how many PSF widths to remove around streak position (overriden by finder)
-        
+        # how many PSF widths to remove around streak position (overriden by finder)
+        self.subtract_psf_widths = 3
+
         # internal switches and parameters
         self.was_expanded = False  # check if original image was expanded before FRT. updated manually from finder
         self.was_convolved = False  # check if original image was convolved with PSF. updated manually from finder
-        self.num_psfs_peak_region = 5  # rough estimate of the region around the Radon peak we want to cut for error estimates
+        # rough estimate of the region around the Radon peak we want to cut for error estimates
+        self.num_psfs_peak_region = 5
         self.num_snr_peak_region = 2  # how many S/N units below maximum is still inside the peak region
-        self.peak_region = None  # a map of the peak region, with only the part above the cut (peak-num_snr_peak_region) not zeroed out (used for error estimates)
+        # a map of the peak region, with only the part above the cut (peak-num_snr_peak_region) not zeroed out (used for error estimates)
+        self.peak_region = None
 
         self._version = 1.03
 
@@ -148,37 +155,40 @@ class Streak:
         else:
             return self.y2 + self.corner_section[0] - self.corner_cutout[0]
 
-
     @property
     def mid_x(self):
         if empty(self.x1) or empty(self.x2):
             return None
         else:
             return (self.x1+self.x2)/2
+
     @property
     def mid_y(self):
         if empty(self.y1) or empty(self.y2):
             return None
         else:
             return (self.y1+self.y2)/2
+
     @property
-    def mid_x_full(self): # this gives the middle x position from the full image
+    def mid_x_full(self):  # this gives the middle x position from the full image
         if empty(self.x1f) or empty(self.x2f):
             return None
         else:
             return (self.x1f+self.x2f)/2
+
     @property
-    def mid_y_full(self): # this gives the middle y position from the full image
+    def mid_y_full(self):  # this gives the middle y position from the full image
         if empty(self.y1f) or empty(self.y2f):
             return None
         else:
             return (self.y1f+self.y2f)/2
 
     def update_from_finder(self, finder):
-        if empty(finder): return
+        if empty(finder):
+            return
 
         for att in self.__dict__.keys():  # load all attributes in "self" that exist in "finder"
-            if hasattr(finder, att) and not callable(getattr(finder,att)):
+            if hasattr(finder, att) and not callable(getattr(finder, att)):
                 setattr(self, att, getattr(finder, att))
 
         self.noise_var = finder.var_scalar
@@ -188,7 +198,7 @@ class Streak:
 
         self.was_convolved = bool(finder.use_conv)
         self.was_expanded = bool(finder.useExpand())
-        
+
     def calculate(self):
         # these are the raw results from this subframe
         self.size_section = self.image_section_proc.shape
@@ -198,24 +208,29 @@ class Streak:
             self.size_section_tr = self.size_section
 
         if not empty(self.image):  # part of the orignal image that is defined as the cutout (without processing!)
-            self.image_section_raw = self.image[self.corner_section[0]:self.size_section[0], self.corner_section[1]:self.size_section[1]]
+            self.image_section_raw = self.image[self.corner_section[0]                                                :self.size_section[0], self.corner_section[1]:self.size_section[1]]
 
-        self.radon_y1 = (2**(self.foldings-1)) * self.radon_max_idx[1]  # size of each slice in y, times the slice number (index)
-        self.radon_y2 = (2**(self.foldings-1)) * (self.radon_max_idx[1]+1)  # same thing, top index (should we include the last pixel?)
-        offset = (self.subframe.shape[2] - self.size_section_tr[1])//2  # this is added on either side if was_expanded
-        self.radon_x0 = self.radon_max_idx[2] - offset  # position of x0 in the subframe (removing the expanded pixels)
-        self.radon_dx = self.radon_max_idx[0] - self.subframe.shape[0]//2  # the angle is in dim0 needs to offset for negative angles
-        
+        # size of each slice in y, times the slice number (index)
+        self.radon_y1 = (2**(self.foldings-1)) * self.radon_max_idx[1]
+        # same thing, top index (should we include the last pixel?)
+        self.radon_y2 = (2**(self.foldings-1)) * (self.radon_max_idx[1]+1)
+        # this is added on either side if was_expanded
+        offset = (self.subframe.shape[2] - self.size_section_tr[1])//2
+        # position of x0 in the subframe (removing the expanded pixels)
+        self.radon_x0 = self.radon_max_idx[2] - offset
+        # the angle is in dim0 needs to offset for negative angles
+        self.radon_dx = self.radon_max_idx[0] - self.subframe.shape[0]//2
+
         # signal to noise from the subframe maximum
         self.snr = self.subframe[self.radon_max_idx]
-        self.is_short = self.subframe.ndim>2 and self.subframe.shape[1]>1
-        
+        self.is_short = self.subframe.ndim > 2 and self.subframe.shape[1] > 1
+
         # assume there is no transpose (then add it if needed)
         self.y1 = self.radon_y1
         self.y2 = self.radon_y2
         self.dy = self.y2-self.y1
         self.dx = self.radon_dx
-        if self.radon_dx!=0:
+        if self.radon_dx != 0:
             self.a = self.dy/self.dx
             self.x0 = self.radon_x0 - self.y1/self.a
             self.b = -self.a*self.x0
@@ -229,19 +244,19 @@ class Streak:
             self.th = 90
             self.x1 = self.radon_x0
             self.x2 = self.radon_x0
-        
+
         self.L = abs(self.radon_dy/math.sin(math.radians(self.th)))
 #        f = math.fabs(math.sin(math.radians(self.th)))
         self.I = self.snr*math.sqrt(self.noise_var*2*math.sqrt(math.pi)*self.psf_sigma/(self.L))
         self.snr_fwhm = self.I*0.81/math.sqrt(self.noise_var)
-        
+
         if self.transposed:
             self.x1, self.y1 = self.y1, self.x1
             self.x2, self.y2 = self.y2, self.x2
             self.a = 1/self.a
             self.b, self.x0 = self.x0, self.b
             self.th = 90 - self.th
-            
+
         # self.x1 = round(self.x1)
         # self.x2 = round(self.x2)
         # self.y1 = round(self.y1)
@@ -250,84 +265,88 @@ class Streak:
 
         ############ calculate the errors on the Radon parameters #############
 
-        R = np.array(self.subframe[:,self.radon_max_idx[1],:]) # present this slice as 2D image
+        R = np.array(self.subframe[:, self.radon_max_idx[1], :])  # present this slice as 2D image
         xmax = self.radon_max_idx[2]
         ymax = self.radon_max_idx[0]
-        
-        S = round(self.psf_sigma*self.num_psfs_peak_region) # size of area of a few PSF widths around the peak
+
+        # size of area of a few PSF widths around the peak
+        S = round(self.psf_sigma*self.num_psfs_peak_region)
         x1 = xmax - S
-        if x1<0: x1 = 0
+        if x1 < 0:
+            x1 = 0
         x2 = xmax + S
-        if x2>=R.shape[1]: x2 = R.shape[1]-1
+        if x2 >= R.shape[1]:
+            x2 = R.shape[1]-1
         y1 = ymax - S
-        if y1<0: y1 = 0
+        if y1 < 0:
+            y1 = 0
         y2 = ymax + S
-        if y2>=R.shape[0]: y2 = R.shape[0]-1
+        if y2 >= R.shape[0]:
+            y2 = R.shape[0]-1
         x1 = int(x1)
         x2 = int(x2)
         y1 = int(y1)
         y2 = int(y2)
-        C = np.array(R[y1:y2,x1:x2]) # make a copy of the array
-        xgrid,ygrid = np.meshgrid(range(C.shape[1]),range(C.shape[0]))
+        C = np.array(R[y1:y2, x1:x2])  # make a copy of the array
+        xgrid, ygrid = np.meshgrid(range(C.shape[1]), range(C.shape[0]))
 
         idx = np.unravel_index(np.nanargmax(C), C.shape)
         mx = C[idx]
-        
-        C[C<mx-self.num_snr_peak_region] = 0
-        
+
+        C[C < mx-self.num_snr_peak_region] = 0
+
         xgrid = xgrid - idx[1]
         ygrid = ygrid - idx[0]
-        
+
         self.radon_x_var = np.sum(C*xgrid**2)/np.sum(C)
-        self.radon_dx_var = np.sum(C*ygrid**2)/np.sum(C) 
+        self.radon_dx_var = np.sum(C*ygrid**2)/np.sum(C)
         self.radon_xdx_cov = np.sum(C*xgrid*ygrid)/np.sum(C)
-        
+
         self.peak_region = C
 
         self.makeCutout()
 
     def makeCutout(self, size=None):
-        
+
         if empty(self.image):
-            return; 
-        
+            return
+
         if empty(size):
-            size = self.cut_size;
-        
+            size = self.cut_size
+
         d1 = int(np.floor(size/2))
         d2 = int(np.ceil(size/2))
-        
+
         x1 = int(round(self.mid_x_full))-d1
         x2 = int(round(self.mid_x_full))+d2
         y1 = int(round(self.mid_y_full))-d1
         y2 = int(round(self.mid_y_full))+d2
-        
-        self.image_cutout = self.image[y1:y2,x1:x2]
-        
+
+        self.image_cutout = self.image[y1:y2, x1:x2]
+
         self.size_cutout = (size, size)
         self.corner_cutout = (y1, x1)
-        
 
     def write_to_disk(self, filename=None):
         if empty(filename):
             if empty(self.filename):
-                return 
-            else: 
+                return
+            else:
                 filename = os.path.splitext(self.filename)[0]+'.h5'
-        
+
         with h5py.File(filename, 'a') as hf:
-            numbers=[int(re.search(r'\d+$', k).group()) for k in hf.keys()]
-            
+            numbers = [int(re.search(r'\d+$', k).group()) for k in hf.keys()]
+
             if empty(numbers):
                 new_number = 1
             else:
                 new_number = max(numbers) + 1
-            
+
             ds = hf.create_dataset('streak_%03d' % (new_number), data=self.image_cutout)
-            
+
             ds.attrs['corner'] = self.corner_cutout
             # add additional metadata later on
-        
+
     def subtractStreak(self, M, replace_value=float('NaN')):
 
         mask = model(imsize(M), self.x1, self.x2, self.y1, self.y2, self.psf_sigma) > 0
@@ -370,23 +389,24 @@ class Streak:
         if ax is None:
             ax = plt.gca()
 
-        if im_type=='section':
+        if im_type == 'section':
             x1 = self.x1
             x2 = self.x2
             y1 = self.y1
             y2 = self.y2
-        elif im_type=='full':
+        elif im_type == 'full':
             x1 = self.x1f
             x2 = self.x2f
             y1 = self.y1f
             y2 = self.y2f
-        elif im_type=='cutout':
+        elif im_type == 'cutout':
             x1 = self.x1c
             x2 = self.x2c
             y1 = self.y1c
             y2 = self.y2c
         else:
-            raise RuntimeError('Unknown im_type: %s. Use "section" or "full" or "cutout".' % ('section'))
+            raise RuntimeError(
+                'Unknown im_type: %s. Use "section" or "full" or "cutout".' % ('section'))
 
         if self.transposed:
             ax.plot([x1, x2], [y1 + offset, y2 + offset], line_format, linewidth=linewidth)
@@ -394,4 +414,3 @@ class Streak:
         else:
             ax.plot([x1 + offset, x2 + offset], [y1, y2], line_format, linewidth=linewidth)
             ax.plot([x1 - offset, x2 - offset], [y1, y2], line_format, linewidth=linewidth)
-
