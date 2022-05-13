10f311d7e692a003c785afa6175dd8aece766138
Guy Nir <guyynir@gmail.com>
2022-05-12 19:39:18 -0700

    cleanup init

--- __init__.py
+++ __init__.py
@@ -1,15 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Dec 21 14:04:16 2017
 
 @author: guyn
 """
-#
-# from pyradon.finder import Finder
-#from pyradon.simulator import Simulator
-#from pyradon.streak import Streak
-
-#import pyradon.simulator
-#import pyradon.finder
-#import pyradon.streak
-#import pyradon.frt
