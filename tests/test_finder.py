d06ae973f3df4014e24dbc44a75eb925afc2f4ed
Guy Nir <guyynir@gmail.com>
2022-06-03 17:54:04 -0700

    improve finder tests

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
@@ -92,6 +93,10 @@ def test_finder_data_psf():
     assert psf_map.shape != data.psf.shape
 
 
+def test_frt_small():
+    pass
+
+
 def test_find_single(f, sim):
     sim.x1 = 3.0 / 8.0
     sim.x2 = 0.5
@@ -105,22 +110,120 @@ def test_find_single(f, sim):
 
     try:
         assert f.streaks
-        assert (
-            abs(f.streaks[0].snr - sim.calc_snr()) / sim.calc_snr() < 0.1
-        )  # 10% differences
-        assert (
-            abs(f.streaks[0].I - sim.intensity) / sim.intensity < 0.1
-        )  # 10% differences
-        assert abs(f.streaks[0].th - sim.th) / sim.th < 0.1  # 10% differences
+        # 10% differences are allowed
+        assert abs(f.streaks[0].snr - sim.calc_snr()) / sim.calc_snr() < 0.1
+        assert abs(f.streaks[0].I - sim.intensity) / sim.intensity < 0.1
+        assert abs(f.streaks[0].th - sim.th) / sim.th < 0.1
         assert (
             abs(f.streaks[0].x0 - sim.x0 * sim.im_size) / (sim.x0 * sim.im_size) < 0.1
-        )  # 10% differences
-        assert (
-            abs(f.streaks[0].L - sim.L * sim.im_size) / (sim.L * sim.im_size) < 0.1
-        )  # 10% differences
+        )
+        assert abs(f.streaks[0].L - sim.L * sim.im_size) / (sim.L * sim.im_size) < 0.1
+
     except Exception as e:
         sim.print()
         f.streaks[0].print()
         f.streaks[0].show()
         plt.show(block=True)
         raise e
+
+
+def test_find_multi(f, sim):
+    x1 = [0.2, 0.6, 0.7, 0.3]
+    x2 = x1[1:] + x1[0:1]
+    y1 = [0.6, 0.7, 0.2, 0.1]
+    y2 = y1[1:] + y1[0:1]
+
+    im = None
+    var = 0
+    sim_vertex = []
+    for i in range(4):
+        sim_vertex.append(copy.copy(sim))
+        sim_vertex[i].x1 = x1[i]
+        sim_vertex[i].x2 = x2[i]
+        sim_vertex[i].y1 = y1[i]
+        sim_vertex[i].y2 = y2[i]
+        sim_vertex[i].make_image()
+        if im is None:
+            im = sim_vertex[i].image
+        else:
+            im += sim_vertex[i].image
+        var += sim_vertex[i].bg_noise_var
+
+    f.data.image = im
+    f.data.psf = sim.psf_sigma
+    f.data.variance = var
+    f.find_multi(f.preprocess(im))
+
+    try:
+        assert len(f.streaks) >= 4
+        sim_th = np.array([s.th for s in sim_vertex])
+        streak_th = [s.th for s in f.streaks]
+        for th in streak_th:
+            angle_diff = 180 - abs(abs(th - sim_th) - 180)
+            assert min(angle_diff) < 5
+
+    except Exception as e:
+        plt.imshow(im)
+        for s in f.streaks:
+            s.show()
+            s.print()
+        for s in sim_vertex:
+            s.print()
+
+        plt.show(block=True)
+        raise e
+
+
+@pytest.mark.flaky(reruns=2)
+def test_scan_thresholds(f, sim):
+    intensities = [100.0, 50.0, 25.0, 10.0]
+    angles = []
+    im = None
+    for i in intensities:
+        sim.intensity = i
+        sim.randomly()
+        sim.make_image()
+        angles.append(sim.th)
+        if im is None:
+            im = sim.image
+        else:
+            im += sim.image
+
+        # TODO: add stars with simulator method
+
+    f.data.image = im
+    f.data.psf = sim.psf_sigma
+    f.data.variance = sim.bg_noise_var * len(intensities)
+    f.scan_thresholds(f.preprocess(im))
+
+    try:
+
+        assert len(f.streaks) >= len(intensities)
+        found_th = np.array([s.th for s in f.streaks])
+        for th in angles:
+            angle_diff = 180 - abs(abs(th - found_th) - 180)
+            assert min(angle_diff) < 5
+
+        max_ratio = 1.5
+        found_in = np.array([s.I for s in f.streaks])
+        for i in intensities:
+            int_ratio = found_in / i
+            assert any(int_ratio < max_ratio) or any(int_ratio > 1 / max_ratio)
+
+    except Exception as e:
+        plt.imshow(im)
+        print(f"simulated angles: {angles}")
+        print(f"simulate intensities: {intensities}")
+        for s in f.streaks:
+            s.show()
+            s.print()
+        plt.show(block=True)
+        raise e
+
+
+def test_gaussian_width():
+    pass
+
+
+def test_jigsaw():
+    pass
